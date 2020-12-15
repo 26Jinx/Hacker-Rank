83e32d485616fdc8d9861ece5c4e3554bb350b54
PharahMain <26randomforests@gmail.com>
2020-12-15 11:53:38 -0800

    solving mod divmod

---
+++ python_math/mod_divmod.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/python-mod-divmod/problem
+
+dividend = int(input())
+divisor = int(input())
+
+res = divmod(dividend, divisor)
+
+print(res[0])
+print(res[1])
+print(res)
